a776fa7ee37d54b3175bd7b98e2064a0a42f987b
Varlotte <32116877+Varlotte@users.noreply.github.com>
2023-08-01 17:01:17 -0400

    passes 4 tests

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
 
-// Write your code here
+let random = Math.floor(Math.random(0) + 10);
+
+let num3 = 29;
+let num4 = 5;
+let mod = num3 % num4;
+
+let max = Math.max(10, 20, 4, 5);
